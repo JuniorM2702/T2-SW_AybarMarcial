b09b2ecea1457401adaddb7ffa58b695bc1006c9
JuniorM2702 <Junior_aybar27@hotmail.com>
2024-06-23 11:32:20 -0500

    service

---
+++ src/main/java/pe/edu/cibertec/T2_SW_AybarMarcial/controller/PublicacionController.java
@@ -0,0 +1,2 @@
+package pe.edu.cibertec.T2_SW_AybarMarcial.controller;public class PublicacionController {
+}
---
+++ src/main/java/pe/edu/cibertec/T2_SW_AybarMarcial/service/PublicacionService.java
@@ -0,0 +1,2 @@
+package pe.edu.cibertec.T2_SW_AybarMarcial.service;public class PublicacionService {
+}
